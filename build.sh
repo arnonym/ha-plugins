d6a7c84f271fbf83cb5b212d4eb5339fe7605759
topic2k <topic2k@atlogger.de>
2023-04-12 19:03:30 +0200

    changes for using dev branch instead of other repo

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-export TMP_URL=~/tmp/ha-plugins-next
+export TMP_URL=~/tmp/ha-plugins-devel
 export REPO_URL=https://github.com/topic2k/ha-plugins
-export NEXT_REPO_URL=https://github.com/topic2k/ha-plugins-next
-export NEXT_REPO_SSH=git@github.com:topic2k/ha-plugins.git
+export REPO_SSH=git@github.com:topic2k/ha-plugins.git
 
 function check_environment () {
     if [ -z "$DOCKER_HUB_PASSWORD" ] || [ -z "$DOCKER_HUB_USER" ]
@@ -14,20 +13,20 @@ function check_environment () {
 }
 
 case "$1" in
-    build-next)
+    build-dev)
         check_environment
-        echo "Building on next repo (aarch64 only)..."
+        echo "Building on development branch (aarch64 only)..."
         rm -rf $TMP_URL
-        git clone -b next $REPO_URL $TMP_URL
+        git clone -b dev $REPO_URL $TMP_URL
         REPO_JSON=$TMP_URL/repository.json
-        contents="$(jq --indent 4 '.name = "Home Assistant addons by Arne Gellhaus (next version) (forked by '$DOCKER_HUB_USER')"' $REPO_JSON)" && echo -E "${contents}" > $REPO_JSON
-        contents="$(jq --indent 4 '.url = env.NEXT_REPO_URL' $REPO_JSON)" && echo -E "${contents}" > $REPO_JSON
+        contents="$(jq --indent 4 '.name = "Home Assistant addons by Arne Gellhaus (development version) (forked by env.DOCKER_HUB_USER)"' $REPO_JSON)" && echo -E "${contents}" > $REPO_JSON
+        contents="$(jq --indent 4 '.url = env.REPO_URL' $REPO_JSON)" && echo -E "${contents}" > $REPO_JSON
         CONFIG_JSON=$TMP_URL/ha-sip/config.json
-        contents="$(jq --indent 4 '.name = "ha-sip-next"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
-        contents="$(jq --indent 4 '.slug = "ha-sip-next"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
-        contents="$(jq --indent 4 '.url = env.NEXT_REPO_URL' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
-        contents="$(jq --indent 4 '.description = "Home-Assistant SIP Gateway (next version)"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
-        contents="$(jq --indent 4 '.image = "'$DOCKER_HUB_USER'/{arch}-ha-sip-next"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
+        contents="$(jq --indent 4 '.name = "ha-sip-dev"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
+        contents="$(jq --indent 4 '.slug = "ha-sip-dev"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
+        contents="$(jq --indent 4 '.url = env.REPO_URL' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
+        contents="$(jq --indent 4 '.description = "Home-Assistant SIP Gateway (development version)"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
+        contents="$(jq --indent 4 '.image = env.DOCKER_HUB_USER/{arch}-ha-sip-dev"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
         if [ -z "$2" ]
           then
             echo "Don't overwrite version."
@@ -37,14 +36,13 @@ case "$1" in
             contents="$(jq --indent 4 '.version = env.OVERWRITE_VERSION' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
         fi
         cd $TMP_URL || exit
-        git commit -a -m "Changes for next repository."
-        git remote set-url origin $NEXT_REPO_SSH
+        git commit -a -m "Changes for development branch."
         git push --force
         docker run \
             --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro \
             homeassistant/amd64-builder:dev \
             --no-cache --aarch64 \
-            -t ha-sip -r $NEXT_REPO_URL -b next \
+            -t ha-sip -r $REPO_URL -b dev \
             --docker-user "$DOCKER_HUB_USER" --docker-password "$DOCKER_HUB_PASSWORD"
         ;;
     build)
@@ -54,7 +52,7 @@ case "$1" in
             --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro \
             homeassistant/amd64-builder:dev \
             --no-cache --all \
-            -t ha-sip -r $REPO_URL -b next \
+            -t ha-sip -r $REPO_URL -b dev \
             --docker-user "$DOCKER_HUB_USER" --docker-password "$DOCKER_HUB_PASSWORD"
         ;;
     update)
@@ -85,7 +83,7 @@ case "$1" in
         python setup.py install
         ;;
     *)
-        echo "Supply one of 'build-next', 'build', 'test', 'update' or 'create-venv'"
+        echo "Supply one of 'build-dev', 'build', 'test', 'update' or 'create-venv'"
         exit 1
         ;;
 esac
