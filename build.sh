fee4269e6eaa49250205eaf04d16e3e668b99640
topic2k <topic2k@atlogger.de>
2023-04-12 18:41:10 +0200

    - use env variable to set docker user - check docker environment variables only when needed

--- build.sh
+++ build.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
 
 export TMP_URL=~/tmp/ha-plugins-next
-export REPO_URL=https://github.com/arnonym/ha-plugins
-export NEXT_REPO_URL=https://github.com/arnonym/ha-plugins-next
-export NEXT_REPO_SSH=git@github.com:arnonym/ha-plugins-next.git
+export REPO_URL=https://github.com/topic2k/ha-plugins
+export NEXT_REPO_URL=https://github.com/topic2k/ha-plugins-next
+export NEXT_REPO_SSH=git@github.com:topic2k/ha-plugins.git
 
-if [ -z "$DOCKER_HUB_PASSWORD" ]
-  then
-    echo "\$DOCKER_HUB_PASSWORD must be set"
-    exit 1
-fi
+function check_environment () {
+    if [ -z "$DOCKER_HUB_PASSWORD" ] || [ -z "$DOCKER_HUB_USER" ]
+    then
+        echo "\$DOCKER_HUB_PASSWORD must be set"
+        exit 1
+    fi
+}
 
 case "$1" in
     build-next)
+        check_environment
         echo "Building on next repo (aarch64 only)..."
         rm -rf $TMP_URL
         git clone -b next $REPO_URL $TMP_URL
         REPO_JSON=$TMP_URL/repository.json
-        contents="$(jq --indent 4 '.name = "Home Assistant addons by Arne Gellhaus (next version)"' $REPO_JSON)" && echo -E "${contents}" > $REPO_JSON
+        contents="$(jq --indent 4 '.name = "Home Assistant addons by Arne Gellhaus (next version) (forked by '$DOCKER_HUB_USER')"' $REPO_JSON)" && echo -E "${contents}" > $REPO_JSON
         contents="$(jq --indent 4 '.url = env.NEXT_REPO_URL' $REPO_JSON)" && echo -E "${contents}" > $REPO_JSON
         CONFIG_JSON=$TMP_URL/ha-sip/config.json
         contents="$(jq --indent 4 '.name = "ha-sip-next"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
         contents="$(jq --indent 4 '.slug = "ha-sip-next"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
         contents="$(jq --indent 4 '.url = env.NEXT_REPO_URL' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
         contents="$(jq --indent 4 '.description = "Home-Assistant SIP Gateway (next version)"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
-        contents="$(jq --indent 4 '.image = "agellhaus/{arch}-ha-sip-next"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
+        contents="$(jq --indent 4 '.image = "'$DOCKER_HUB_USER'/{arch}-ha-sip-next"' $CONFIG_JSON)" && echo -E "${contents}" > $CONFIG_JSON
         if [ -z "$2" ]
           then
             echo "Don't overwrite version."
@@ -42,16 +45,17 @@ case "$1" in
             homeassistant/amd64-builder:dev \
             --no-cache --aarch64 \
             -t ha-sip -r $NEXT_REPO_URL -b next \
-            --docker-user agellhaus --docker-password "$DOCKER_HUB_PASSWORD"
+            --docker-user "$DOCKER_HUB_USER" --docker-password "$DOCKER_HUB_PASSWORD"
         ;;
     build)
+        check_environment
         echo "Building prod (all archs)..."
         docker run \
             --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro \
             homeassistant/amd64-builder:dev \
             --no-cache --all \
             -t ha-sip -r $REPO_URL -b next \
-            --docker-user agellhaus --docker-password "$DOCKER_HUB_PASSWORD"
+            --docker-user "$DOCKER_HUB_USER" --docker-password "$DOCKER_HUB_PASSWORD"
         ;;
     update)
         echo "Updating builder..."
